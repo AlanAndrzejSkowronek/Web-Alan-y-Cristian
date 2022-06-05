e0892619d2c63d5655b79e33b8724fa47c7016c7
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-06-05 11:10:07 +0200

    mejoras menu principal responsive

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,21 +1,59 @@
 let contSubM = document.getElementsByClassName("contSubM");
-let prueba=document.querySelector("h1");
-let activar = true;
-function alternarMenuNoticia() {
-    if (activar) {
-        activarMenu();
-        activar = false;
+let contMenu=document.querySelector("nav");
+let contMenuMobilcss=document.getElementById("contMenuMobilcss");
+let subMActivado = true;
+let mActivado=true;
+let logoMenuMoblilVisible=false;
+//subMenu
+function alternarSubMenuNoticia() {
+    if (subMActivado) {
+        activarSubMenu()
     } else {
-        desactivarMenu();
-        activar = true;
+        desactivarSubMenu()
     }
 }
-function activarMenu() {
+function activarSubMenu() {
     contSubM[0].style.display = "block";
+    subMActivado = false;
 }
-function desactivarMenu() {
+function desactivarSubMenu() {
     contSubM[0].style.display = "none";
+    subMActivado=true;
 }
-function b(){
-   prueba.style.color="red"
+
+//menu
+function alternarMenu(){
+    if (mActivado) {
+        insertarMenuMobilcss()
+        activarMenu()
+    } else {
+        desactivarMenu()
+    }
+
+}
+function activarMenu(){
+    contMenu.style.display = "block";
+    mActivado = false;
+}
+function desactivarMenu(){
+    contMenu.style.display = "none";
+    mActivado = true;
+}
+//menuMobil.css
+function insertarMenuMobilcss(){
+    contMenuMobilcss.innerHTML='<link rel="stylesheet" href="./css/menuMobil.css">';
+}
+function eliminarMenuMobilcss(){
+    contMenuMobilcss.innerHTML='';
+}
+function logoMenuMobilVisible(){
+if ($("#menuMobil").is(":visible")) {
+    desactivarMenu()
+    insertarMenuMobilcss()
+    return true;
+  }else{ 
+      activarMenu();
+    eliminarMenuMobilcss()
+     return false;
+  }
 }
